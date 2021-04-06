73a0992734397c0f85bbe79592d64af547eb1f72
BYU-IVY <byu.ivy.capstone@gmail.com>
2021-04-06 09:34:58 +0000

    Finished formatting for excel sheet and implemented criticality grade

--- ivy_api/services/makeCsv.js
+++ ivy_api/services/makeCsv.js
@@ -3,21 +3,55 @@ const helper = require('../helper');
 const config = require('../config');
 
 const excel = require("exceljs");
-const answersJSON = { answers:[{QID: 1,  Answer: "Yes"}, {QID: 2,  Answer: "Yes"}, {QID: 3,  Answer: "Yes"}, {QID: 4,  Answer: "Yes"}, {QID: 5,  Answer: "Yes"}, {QID: 6,  Answer: "Yes"}, {QID: 7,  Answer: "Yes"}, {QID: 8,  Answer: "Yes"}, {QID: 9,  Answer: "Yes"}, {QID: 10,  Answer: "Yes"}, {QID: 11,  Answer: "Yes"}, {QID: 12,  Answer: "Yes"}, {QID: 13,  Answer: "Yes"}, {QID: 14,  Answer: "Yes"}, {QID: 15,  Answer: "Yes"}, {QID: 16,  Answer: "Yes"}, {QID: 17,  Answer: "Yes"}, {QID: 18,  Answer: "Yes"}, {QID: 19,  Answer: "Yes"}, {QID: 20,  Answer: "Yes"}, {QID: 21,  Answer: "Yes"}, {QID: 22,  Answer: "Yes"}]}
+//const answersJSON = { answers:[{QID: 1,  Answer: "Yes"}, {QID: 2,  Answer: "Yes"}, {QID: 3,  Answer: "Yes"}, {QID: 4,  Answer: "Yes"}, {QID: 5,  Answer: "Yes"}, {QID: 6,  Answer: "Yes"}, {QID: 7,  Answer: "Yes"}, {QID: 8,  Answer: "Yes"}, {QID: 9,  Answer: "Yes"}, {QID: 10,  Answer: "Yes"}, {QID: 11,  Answer: "Yes"}, {QID: 12,  Answer: "Yes"}, {QID: 13,  Answer: "Yes"}, {QID: 14,  Answer: "Yes"}, {QID: 15,  Answer: "Yes"}, {QID: 16,  Answer: "Yes"}, {QID: 17,  Answer: "Yes"}, {QID: 18,  Answer: "Yes"}, {QID: 19,  Answer: "Yes"}, {QID: 20,  Answer: "Yes"}, {QID: 21,  Answer: "Yes"}, {QID: 22,  Answer: "Yes"}]}
 
 async function download (req, res) {
-	//console.log(res)
 
-	const answers = answersJSON.answers;
+
+	answerJSON = Array.from(req.query.answersToQuestions);
+	 
+	console.log(answerJSON)
+	//const answers = answersJSON.answers;
 	var qids = "";
-	for(let i = 0; i < answers.length; i++){
-		if (answers[i].Answer == "Yes"){ 
-			qids += answers[i].QID ;
-			qids += ",";
+	var totalCriticality = 1;
+	var nonCompliantCriticality = 1;
+	for(let i = 0; i < answerJSON.length; i++){
+		obj = JSON.parse(answerJSON[i]);
+		console.log(obj);
+		totalCriticality += obj.Criticality;
+		//console.log(answerJSON[i].Answer);
+		//console.log(answerJSON[i].CompliantAnswer);
+		if (obj.Answer != obj.CompliantAnswer) {
+			if (obj.CompliantAnswer != "") {
+				qids += obj.QID;
+				qids += ",";
+				nonCompliantCriticality += obj.Criticality;
+			}
 		}
 	}
 	qids = qids.slice(0,-1);
 
+	console.log(qids);
+	console.log(nonCompliantCriticality);
+	console.log(totalCriticality);
+
+	var score = (totalCriticality - nonCompliantCriticality) / totalCriticality;
+
+	console.log(score);
+	var grade = "";
+	if(score >= .9)
+		grade = "A"
+	if(.9 > score && score >= .8)
+		grade = "B"
+	if(.8 > score && score >= .7)
+		grade = "C"
+	if(.7 > score && score >= .6)
+		grade = "D"
+	if(.6 > score && score >= .5)
+		grade = "F"
+	if(.5 > score)
+		grade = "Critical"
+	
 	//const query = 'SELECT Name, QID, ControlID, FrameworkID, Description, QTEXT, MoreDetail FROM `Question_Control` JOIN Control ON Control.ConrolID = Question_Control.ControlID JOIN Question ON Question.QID = Question_Control.QuestionID IN (' + qids + ')'
 
 	const query = 'SELECT Name, QID, ControlID, FrameworkID, QTEXT, Description, MoreDetail FROM `Question_Control` INNER JOIN Question ON Question_Control.QuestionID = Question.QID INNER JOIN Control ON Question_Control.ControlID = Control.ConrolID WHERE QID IN (' + qids +')'
@@ -28,42 +62,53 @@ async function download (req, res) {
 	let workbook = new excel.Workbook();
 	let worksheet = workbook.addWorksheet("Controls");
 
+	/*TITLE*/
+	worksheet.mergeCells('A1', 'D6');
+	worksheet.getCell('C2').value = 'ARMOR SECURITY COMPLIANCE REPORT';
+	worksheet.getRow(1).alignment = {vertical: 'middle', wrapText: true};
+	worksheet.getCell('C2').fill = { type: 'pattern', pattern: 'solid', fgColor:{argb:'44546a'}, bgColor:{argb:'44546a'} };
+	worksheet.getCell('A1').font = {size: 36, name: 'calibri', color: {argb: 'fdfdfd'}, bold: true};
+	worksheet.addRow({ }).commit();
+	/*Column headers*/
 
-	/*Tutorial.findAll().then((objs) => {
-		let tutorials = [];
+	worksheet.mergeCells('A7', 'A11');
+	worksheet.getCell('A8').value = 'Your Security Grade is: ';
+	worksheet.getCell('A8').font = {size: 18, bold: true};
+	worksheet.getCell('A8').fill = { type: 'pattern', pattern: 'solid', fgColor:{argb:'BFBFBF'}, bgColor:{argb:'BFBFBF'} };
+	
+	var gradeColor = "";
 
-		objs.forEach((obj) => {
-			tutorials.push({
-				id: obj.id,
-				title: obj.title,
-				description: obj.description,
-				published: obj.published,
-			});
-		});
+	if(grade == "A")
+		gradeColor = '00FF00';
+	if(grade == "B")
+		gradeColor = 'C0FB05';
+	if(grade == "C")
+		gradeColor = 'EDFF01';
+	if(grade == "D")
+		gradeColor = 'FFA401';
+	if(grade == "F")
+		gradeColor = 'F95D07';
+	if(grade == 'Critical')
+		gradeColor = 'FF0000';
 
-		let workbook = new excel.Workbook();
-		let worksheet = workbook.addWorksheet("Tutorials");
+	console.log(gradeColor);
 
-		worksheet.columns = [
-			{ header: "Id", key: "id", width: 5 },
-			{ header: "Title", key: "title", width: 25 },
-			{ header: "Description", key: "description", width: 25 },
-			{ header: "Published", key: "published", width: 10 },
-		];
+	worksheet.mergeCells('B7', 'D11');
+	worksheet.getCell('B8').value = grade;
+	worksheet.getCell('B8').font = {size: 36, color: {argb: gradeColor}, bold: true};
+	worksheet.getCell('B8').fill = { type: 'pattern', pattern: 'solid', fgColor:{argb:'BFBFBF'}, bgColor:{argb:'BFBFBF'} };
 
-		// Add Array Rows
-		worksheet.addRows(controls);
-
-	});*/
+	worksheet.getRow(11).values = ['Security Control Name', 'Question', 'Control Description', 'Action'];
+	worksheet.getRow(11).height = 34;
+	worksheet.getRow(11).font = {bold: true}
 
 	worksheet.columns = [
-		{ header: "Security Control Name", key: "Name", width: 25, height: 30 },
-		{ header: "Question", key: "QTEXT", width: 100, height: 30 },
-		{ header: "Control Description", key: "Description", width: 100, height: 30 },
-		{ header: "Action", key: "MoreDetail", width: 100, height: 30 },
-		
+		{ key: "Name", width: 25, height: 30 },
+		{ key: "QTEXT", width: 90, height: 30 },
+		{ key: "Description", width: 90, height: 30 },
+		{ key: "MoreDetail", width: 90, height: 30 },
 	];
-	worksheet.getRow(1).font = {bold: true}
+
 	worksheet.addRows(controls);
 
 	let rowIndex = 1;
--- ivy_prototype/src/SurveyQuestions.js
+++ ivy_prototype/src/SurveyQuestions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react'
-import { Progress, Segment, Button, Grid, Label, Icon} from 'semantic-ui-react'
+import { Progress, Segment, Button, Grid, Label, Icon, Modal, Header, Sidebar, Menu, Image} from 'semantic-ui-react'
+
 
 
 export default class SurveyQuestions extends Component {
@@ -10,49 +11,90 @@ export default class SurveyQuestions extends Component {
             questionInArray: 0,
             currentQuestionText: null,
             currentQuestionID: null,
+            currentCompliantAnswer:null,
+            currentCriticality:null,
             questionaireLenght: 0,
-            answerToPreviousQuestion: null
+            questions: null,
+            answersToExcel: []
         }
         this.loadQuestions()
     }
 
+    populateToExcel = (QID, answer, compliantAnswer, criticality) => {
+        var questionAnswers = {"QID":QID,"Answer":answer, "CompliantAnswer":compliantAnswer, "Criticality":criticality}
+        var answersToExcelCopy = this.state.answersToExcel
+        if (answersToExcelCopy[this.state.questionInArray]) {
+            answersToExcelCopy.splice(this.state.questionInArray, 1, questionAnswers)
+        } else {
+            answersToExcelCopy.push(questionAnswers)
+        }
+        this.setState ({
+            answersToExcel: answersToExcelCopy
+        })
+        console.log(this.state.answersToExcel)
+    }
+
 
     yesAnswer = () => {
-        var questionanswer = {"QID":this.state.currentQuestionID,"response":"yes"}
-        console.log(questionanswer)
+        this.populateToExcel(this.state.currentQuestionID, "Yes", this.state.currentCompliantAnswer,this.state.currentCriticality)
         var nextQuestionInArray = this.state.questionInArray + 1
+        this.loadFollowup(nextQuestionInArray)
         this.setState({
             questionInArray: nextQuestionInArray,
-            answerToPreviousQuestion: questionanswer
         })
-        this.loadQuestions()
+        this.displayQuestion(nextQuestionInArray)
     }
 
     noAnswer = () => {
-        var questionanswer = {"QID":this.state.currentQuestionID,"response":"no"}
+        this.populateToExcel(this.state.currentQuestionID, "No", this.state.currentCompliantAnswer,this.state.currentCriticality)
         var nextQuestionInArray = this.state.questionInArray + 1
         this.setState({
             questionInArray: nextQuestionInArray,
-            answerToPreviousQuestion: questionanswer
         })
-        this.loadQuestions()
+        this.displayQuestion(nextQuestionInArray)
     }
+
 
     previousQuestion = () => {
         var previousQuestionInArray = this.state.questionInArray - 1
         this.setState({
             questionInArray: previousQuestionInArray
         })
-        this.loadQuestions()
+        this.displayQuestion(previousQuestionInArray)
     }
 
     printBackButton = () => {
         if (this.state.questionInArray > 0) {
             return (
                 <Button onClick={this.previousQuestion} circular positive size='massive' floated='left' icon style={{background: '#373737', width: '100px'}}>
-                    <Icon color='white' name='arrow left' />
+                    <Icon name='arrow left' />
                 </Button>
             )
+        }
+    }
+
+    displayQuestion = (nextQuestionInArray) => {
+        if (this.state.questions[nextQuestionInArray]) {
+            var question = ""
+            var QID = ""
+            var compliantAnswer = ""
+            var criticality = ""
+            question = this.state.questions[nextQuestionInArray].QText
+            QID = this.state.questions[nextQuestionInArray].QID
+            compliantAnswer = this.state.questions[nextQuestionInArray].CompliantAnswer
+            criticality = this.state.questions[nextQuestionInArray].Criticality
+            console.log(question)
+            this.setState ({
+                currentQuestionText: question,
+                currentQuestionID: QID,
+                currentCompliantAnswer:compliantAnswer,
+                currentCriticality:criticality
+            })
+        } else {
+            this.setState ({
+                currentQuestionText: "",
+                currentQuestionID: -1
+            })
         }
     }
 
@@ -62,12 +104,11 @@ export default class SurveyQuestions extends Component {
         this.setState ({
             currentQuestionText: ""
         })
-        axios.get("/questions", {
-            params: {
-                previousQuestionAnswer: this.state.answerToPreviousQuestion
-            }
-        }).then(response => {
+        axios.get("/questions").then(response => {
             console.log(response)
+            this.setState ({
+                questions: response.data.data
+            })
             if (response.data.data[this.state.questionInArray]) {
                 question = response.data.data[this.state.questionInArray].QText
                 QID = response.data.data[this.state.questionInArray].QID
@@ -90,13 +131,64 @@ export default class SurveyQuestions extends Component {
         })
     }
 
+    loadFollowup = (ParentQID) => {
+        var allQuestions = this.state.questions
+        console.log(allQuestions)
+        var url = "/follow/" + ParentQID
+        axios.get(url).then(response => {
+            console.log(response.data.data)
+            var followUpQuestions = response.data.data
+            for (var followUp = 0; followUp < followUpQuestions.length; followUp++) {
+                for (var questionNumber = 0; questionNumber < allQuestions.length; questionNumber++) {
+                    console.log(allQuestions[questionNumber.QID])
+                    if(allQuestions[questionNumber + 1]) {
+                        if (allQuestions[questionNumber].QID < followUpQuestions[followUp].QID && allQuestions[questionNumber + 1].QID > followUpQuestions[followUp].QID) {
+                            console.log('insert')
+                            allQuestions.splice(questionNumber + 1, 0, followUpQuestions[followUp])
+                            console.log(allQuestions)
+                            break
+                        }
+                    } else {
+                        allQuestions.push(followUpQuestions[followUp])
+                        console.log(allQuestions)
+                        break
+                    }
+                }
+            }        
+        }).catch(error => {
+            console.log("no response from server")
+            
+        })
+    }
+
+    DisplayMoreDetails = () => {
+        if (this.state.questions && this.state.questions[this.state.questionInArray].MoreDetail) {
+            return (
+            <Modal style={{background: '#707070'}} trigger={<Button circular positive style={{background: '#2F2F2F'}}>More Details</Button>}>
+            <Header content='Details' />
+            <Modal.Content>
+                
+                <p>
+                {this.state.questions[this.state.questionInArray].MoreDetail}
+                </p>
+            </Modal.Content>
+            </Modal>
+            )
+        }
+    }
+
     downloadCVE = () => {
         axios({
             url: '/excel',
             method: 'GET',
             responseType: 'blob', // important
+            params: {
+                answersToQuestions: this.state.answersToExcel
+            }
           }).then((response) => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
+	    console.log(response.data);
+	    console.log(this.state.answersToExcel);
             const link = document.createElement('a');
             link.href = url;
             link.setAttribute('download', 'SecurityPosture.xlsx');
@@ -105,47 +197,97 @@ export default class SurveyQuestions extends Component {
           });
     }
 
+    loadSideBarMenu = () => {
+        for (var i = 0; i < this.state.questions.length; i++) {
+            if (this.state.questions[i].QSummary) {
+                return (
+                    <Menu.Item as='a'>
+                    {this.state.questions[i].QSummary}
+                    </Menu.Item>
+                )
+            }
+        }
+    }
+
+    displaySidebar = () => {
+        return (
+            <div>
+                <Sidebar
+                    as={Menu}
+                    animation='overlay'
+                    icon='labeled'
+                    inverted
+                    vertical
+                    visible={true}
+                    width='thin'
+                >
+                    <Menu.Item as='a'>
+                    Home
+                    </Menu.Item>
+                    <Menu.Item as='a'>
+                    Games
+                    </Menu.Item>
+                    <Menu.Item as='a'>
+                    Channels
+                    </Menu.Item>
+                </Sidebar>
+            </div>
+        )
+    }
+
+
 
     render() {
         if (this.state.currentQuestionID >= 0) {
             return (
                 <div>
+                    {this.displaySidebar()}
+
+                    <Sidebar.Pusher>
                     <Grid>
                         <Grid.Row style={{marginBottom: '1vh', display: "flex", justifyContent: "center", alignItems: "center"}}>
                             <Grid.Column width={2}>
-                                <Label circular size="massive" style={{background:'#34FFC8'}}>{this.state.currentQuestionID}</Label>
+                                <Label circular size="massive" style={{background:'#34FFC8'}}>{this.state.questionInArray + 1}</Label>
                             </Grid.Column>
                             <Grid.Column width={12}>
-                                <Progress value={this.state.questionInArray} total={this.state.questionaireLenght} inverted size='tiny' style={{marginTop: '2vh'}}/>
+                                <Progress value={this.state.currentQuestionID} total={111} inverted size='tiny' style={{marginTop: '2vh'}}/>
                             </Grid.Column>
                             <Grid.Column width={2}/>
                         </Grid.Row>
                     </Grid>
-                    <Segment style={{background: '#373737', height: '50vh', display: "flex", justifyContent: "center", alignItems: "center", marginTop: "3vh"}}>
-                        <b style={{color: 'white', fontSize: '30px', lineHeight: '130%', padding: '10vh'}}>{this.state.currentQuestionText}</b>
+                    <Segment style={{background: '#373737', height: '50vh', marginTop: "3vh"}}>
+                        <Grid>
+                            <Grid.Row style={{display: "flex", justifyContent: "center", alignItems: "center", height: '40vh'}}>
+                                <b style={{color: 'white', fontSize: '30px', lineHeight: '130%', padding: '10vh'}}>{this.state.currentQuestionText}</b>
+                            </Grid.Row>
+                            <Grid.Row style={{display: "flex", justifyContent: "flex-end", alignItems: "flex-end", marginRight: '5vh'}}>
+                            {this.DisplayMoreDetails()}
+                            </Grid.Row>
+                        </Grid>
                     </Segment>
                     <Grid>
-                            <Grid.Column width={3}>
-                                <Grid columns={1} style={{marginTop: '3vh'}}>
+                        <Grid.Column width={3}>
+                            <Grid columns={1} style={{marginTop: '3vh'}}>
+                                <Grid.Column>
+                                    {this.printBackButton()}
+                                </Grid.Column>
+                            </Grid>
+                        </Grid.Column>
+                            <Grid.Column width={10}>
+                                <Grid columns={2} style={{marginTop: '3vh'}}>
                                     <Grid.Column>
-                                        {this.printBackButton()}
+                                        <Button onClick={this.noAnswer} circular positive size='massive' style={{background: '#373737', width: '200px'}}>No</Button>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Button onClick={this.yesAnswer} circular positive size='massive' style={{background: '#373737', width: '200px'}}>Yes</Button>
                                     </Grid.Column>
                                 </Grid>
-                            </Grid.Column>
-                                <Grid.Column width={10}>
-                                    <Grid columns={2} style={{marginTop: '3vh'}}>
-                                        <Grid.Column>
-                                            <Button onClick={this.noAnswer} circular positive size='massive' style={{background: '#373737', width: '200px'}}>No</Button>
-                                        </Grid.Column>
-                                        <Grid.Column>
-                                            <Button onClick={this.yesAnswer} circular positive size='massive' style={{background: '#373737', width: '200px'}}>Yes</Button>
-                                        </Grid.Column>
-                                    </Grid>
-                                </Grid.Column>  
-                            <Grid.Column width={3}>
-                            </Grid.Column>    
-                            
-                        </Grid>
+                            </Grid.Column>  
+                        <Grid.Column width={3}>
+                        </Grid.Column>    
+                        
+                    </Grid>
+                    </Sidebar.Pusher>
                 </div>
             )
         } else {
